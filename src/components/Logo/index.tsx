2bd020780c0762131e57896cb31dea10d40d33bf
Lucas Santana <122113277+llink7@users.noreply.github.com>
2023-07-27 14:02:48 -0300

    refactor: logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,14 +1,11 @@
 import { LogoContainer, ImgContainer } from "./styles";
-import logoPizza from "../../assets/images/logoPizza-PhotoRoom.png-PhotoRoom.png";
 import { Link } from "react-router-dom";
 import LogoTitle from "../LogoTitle";
 export default function Logo() {
   return (
     <Link to="/">
       <LogoContainer>
-        <ImgContainer>
-          <img src={logoPizza} alt="" />
-        </ImgContainer>
+        
         <LogoTitle />
       </LogoContainer>
     </Link>
